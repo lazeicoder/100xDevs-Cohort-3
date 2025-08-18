f570bb614cc504ee340bddfbe9481e7bbca427cf
lazeicoder <anuragmukherjeedev@gmail.com>
2025-08-18 22:58:46 +0530

    Added JWT based Authentication

--- Week-6/6.1-HTTP Deep Dive/server.js
+++ Week-6/6.1-HTTP Deep Dive/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'jwt@secret';
+
 const app = express();
+
+const users = [];
 
 app.use(express.json());
 
@@ -35,17 +39,47 @@ app.post('/signin', (req, res) => {
     const user = users.find(user => user.username === username && user.password === password);
 
     if (user) {
-        
+        // Converting the username into a jwt
+        const token = jwt.sign({
+            username: username 
+        }, JWT_SECRET);
+
+        // user.token = token;
+
+        res.send({
+            msg: "You're signed in!",
+            token: token 
+        });
     } else {
         res.send({
             msg: "Unauthorized credentials."
         });
     }
+
+    console.log(users);
 });
 
 app.get('/me', (req, res) => {
     const token = req.headers.token;
+
+    // This will get back the json object => {username, password}.
+    const decodedInfo = jwt.verify(token, JWT_SECRET);
+
+    const username = decodedInfo.username;
+
+    const user = users.find(user => user.username === username);
+
+    if (user) { 
+        res.json({
+            username: username,
+            password: user.password 
+        });
+    }
 });
+
+app.get('/admin', (req, res) => {
+    res.send(users);
+})
 
 app.listen(3000, () => {
     console.log(`Server is live on Port 3000`);
